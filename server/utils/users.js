7d4d964a03dd1505897418509f1c69c6eb295ae8
Fernando Augusto <fernando@MacBook-Pro-de-Fernando.local>
2017-10-17 23:08:16 -0200

    Send messages to only people in some room

---
+++ server/utils/users.js
@@ -0,0 +1,53 @@
+[{
+  id: '#15654sdf465',
+  name: 'Fer',
+  room: 'The Office Fans'
+}]
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => {
+      return user.room === room;
+    });
+    var namesArray = users.map((user) => {
+      return user.name;
+    });
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
+
+// class Person {
+//   constructor (name, age) {
+//     this.name = name;
+//     this.age = age
+//   }
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} years(s) old.`;
+//   }
+// }
+//
+// var me = new Person('Fer', 25);
+// var description = me.getUserDescription();
+// console.log(description);
